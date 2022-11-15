3ff5d5ca4a525f97830df5233355f59126108af4
GabrielCanalha <gabrielarruda.1504@gmail.com>
2022-11-14 22:32:04 -0300

    Primeiro Commit ao git para fins de colaboração no PJandeson.

---
+++ src/pages/Home/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+/*const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;*/
